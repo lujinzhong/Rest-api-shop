6ee5753a047b4a48c209e4eb6fea3876120a9e06
lujinzhong <15626425282@163.com>
2019-03-06 13:34:32 +0800

    v1.0.1
    屏蔽业务信息

--- application/extra/wx.php
+++ application/extra/wx.php
@@ -6,8 +6,8 @@
  * Time: 13:30
  */
 return [
- 'app_id'=>'wxe53979885c7f744e',
- 'app_secret'=>'010a3df7fe7061323b84fc32b95c65b9',
+ 'app_id'=>'appid',
+ 'app_secret'=>'app_secret',
  'login_url'=>'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
 
 ];
